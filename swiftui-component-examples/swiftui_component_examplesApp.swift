3282f0fc2a1eea50b066705ea5bea1eaf1ad7996
Andreas <dablnazero@gmail.com>
2023-02-27 13:23:07 +0300

    Initial Commit

---
+++ swiftui-component-examples/swiftui_component_examplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_component_examplesApp.swift
+//  swiftui-component-examples
+//
+//  Created by Andreas on 27.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_component_examplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
